c045df6f1ff97afb5e7708fed2737553f9254b38
barks1212 <willbarkerwork@gmail.com>
2019-08-14 16:58:45 +0100

    config tabs function to switch tabs, call on window load

--- tabs.js
+++ tabs.js
@@ -1,1 +1,21 @@
-console.log('log from tabs.js');
+const configTabs = () => {
+    document.querySelectorAll('.tabButton').forEach(button => {
+        button.addEventListener('click', () => {
+            
+            const tabId = button.dataset.tabButton;
+            const container = button.parentElement.parentElement
+            const toBecomeActive = container.querySelector(`.content[tab="${tabId}"]`);
+
+            button.parentElement.querySelectorAll('.tabButton').forEach(button => {
+                button.classList.remove('active');
+            })
+            container.querySelectorAll('.content').forEach(content => {
+                content.classList.remove('activeContent');
+            })
+            button.classList.add('active');
+            toBecomeActive.classList.add('activeContent');
+        })
+    })
+}
+
+window.onload = configTabs;
